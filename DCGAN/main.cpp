82baca0736a8086b88b6fa094c156f7654b865b1
krshrimali <kushashwaravishrimali@gmail.com>
2019-08-19 23:40:56 +0530

    Initial Commit

---
+++ DCGAN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DCGAN
+//
+//  Created by Kushashwa Ravi Shrimali on 19/08/19.
+//  Copyright © 2019 Kushashwa Ravi Shrimali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
