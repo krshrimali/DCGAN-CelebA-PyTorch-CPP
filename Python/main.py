c6b890a94f54f1f742a36a21ef9c927366ef9e37
krshrimali <kushashwaravishrimali@gmail.com>
2019-10-07 15:41:25 +0530

    line 85 problem with is_cuda()

--- Python/main.py
+++ Python/main.py
@@ -82,10 +82,10 @@ class Discriminator(torch.nn.Module):
             torch.nn.Sigmoid()
         )
     def forward(self, input):
-        if input.is_cuda() and self.ngpu > 1:
-            output = torch.nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else:
-            output = self.main(input)
+        # if input.is_cuda() and self.ngpu > 1:
+        # output = torch.nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+        # else:
+        output = self.main(input)
         return output.view(-1, 1).squeeze(1)
 
 if __name__ == "__main__":
