aa2993b3b2d9e9443a74348ba348cf58e6e3cdf6
Himanshu <heman.sam@gmail.com>
2020-03-31 11:40:21 +0800

    removed colors , created meaningful directory names and added path for OPENCV in cmake

---
+++ automate.sh
@@ -0,0 +1,33 @@
+#! /bin/sh
+
+# Variables for folder names and paths 
+BUILD_F=build
+OUTPUT_F=build/output
+
+PATH_LIBTORCH_DIR=/root/libtorch
+PATH_LIBTORCH_BUILD=/root//libtorch/share/cmake/Torch/
+PATH_OPENCV_BUILD=/root/opencv/build/
+
+
+if [ -d $BUILD_F ] ; then
+	echo "build folder Already Present... !! skipping folder creation"
+else
+	echo "Creating build folder"
+	mkdir build && echo "Done" || (echo "Folder creation failed.. exiting..." && exit 0)
+fi
+
+
+echo "Running Cmake inside the build folder..."
+(cd build && cmake -DCMAKE_PREFIX_PATH=${PATH_LIBTORCH_DIR} -DCMAKE_MODULE_PATH=${PATH_LIBTORCH_BUILD} -DOpenCV_DIR=${PATH_OPENCV_BUILD} ..) || (echo "${bold}${red}cmake command failed... exiting.." && exit 0)
+
+
+if [ -d ${OUTPUT_F} ] ; then
+	echo "output folder already exists inside build...!! skipping folder creation"
+else
+	echo "Creating output folder inside build"
+	mkdir -p build/output && echo "Done" || (echo "Folder creation failed.. exiting" && exit 0)
+fi
+
+echo "Running the make command"
+
+(cd build && make) && echo "Done" || (echo "make command failed.. exiting ..." && exit 0)
